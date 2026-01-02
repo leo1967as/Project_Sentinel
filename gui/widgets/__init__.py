b7b5ae5b452b782780c7ef0ef7794cd94662f01c
leo1967as <narawhich2547@gmail.com>
2026-01-03 00:27:37 +0700

    fix: Add PnLDisplayWidget to widgets package export

--- gui/widgets/__init__.py
+++ gui/widgets/__init__.py
@@ -6,6 +6,7 @@ from .connection_card import ConnectionCard
 from .quick_stats import QuickStatsCard
 from .control_panel import ControlPanel
 from .pnl_gauge import PnLGaugeWidget
+from .pnl_display import PnLDisplayWidget
 from .positions_table import PositionsTableWidget
 from .mode_indicator import ModeIndicatorWidget
 from .tick_stats import TickStatsWidget
@@ -17,6 +18,7 @@ __all__ = [
     'QuickStatsCard', 
     'ControlPanel',
     'PnLGaugeWidget',
+    'PnLDisplayWidget',
     'PositionsTableWidget',
     'ModeIndicatorWidget',
     'TickStatsWidget',
